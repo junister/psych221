2f3a3ee53fe965ea9fc1a656d1c731f01e749054
Brian Wandell <wandell@stanford.edu>
2021-07-22 18:16:17 -0700

    small updates from V3

--- tutorials/v4/introduction/t_piIntro_pbrtv4.m
+++ tutorials/v4/introduction/t_piIntro_pbrtv4.m
@@ -1,5 +1,26 @@
 %% pbrt v4 introduction 
-% CPU only%
+%
+% OpenExr libraries are needed for the matlab openexr MEX file.
+% The issue is caused by openexr version…, 
+% Use this version openexr2.5.3:
+% https://github.com/AcademySoftwareFoundation/openexr/archive/refs/tags/v2.5.3.zip
+% To install, download and unzip and in a terminal run
+%   mkdir build
+%   cd build
+%   cmake ../
+%   make
+%   make install
+%
+% Then go to ISET3d=v4/external/openexr and create the MEX file
+%   make
+%
+% This produces several mex files that we use for reading the rendered data
+%
+%  exrinfo.mexmaci64            exrreadchannels.mexmaci64
+%  exrwritechannels.mexmaci64   exrread.mexmaci64
+%  exrwrite.mexmaci64   
+% 
+% CPU only
 % blender uses a coordinate system like this:
 %    
 %                 z
@@ -8,6 +29,11 @@
 %                 |  
 %                 x - - - - >y
 % unit scale uses centermeter by default
+%
+% TO CHECK for updates
+%    tree
+%    piWRS
+%   
 %% Init
 ieInit;
 if ~piDockerExists, piDockerConfig; end
@@ -40,12 +66,8 @@ thisR.set('film render type',{'radiance','depth'})
 thisR.set('asset','Cylinder.001_B','world translation',[0.2 0 0]);
 
 %% write the data out
-piWrite(thisR);
 
-%% render the scene
-
-[scene,result] = piRender(thisR);
-sceneWindow(scene);
+scene = piWRS(thisR);
 
 %{
 scene = piRenderCloud(thisR);
---
+++ utilities/piWRS.m
@@ -0,0 +1,38 @@
+function [obj,results] = piWRS(thisR,varargin)
+% Write, render, show radiance image
+%
+% Write, Render, Show a scene specified by a recipe
+%
+% Synopsis
+%   [isetObj, results] = piWRS(thisR)
+% 
+% See also
+%   piRender, sceneWindow, oiWindow
+
+%%
+varargin = ieParamFormat(varargin);
+
+p = inputParser;
+p.addRequired('thisR',@(x)(isa(x,'recipe')));
+p.addParameter('dockerimagename','camerasimulation/pbrt-v4-cpu',@ischar);
+p.addParameter('rendertype','radiance',@ischar);
+
+p.parse(thisR,varargin{:});
+thisDocker = p.Results.dockerimagename;
+renderType = p.Results.rendertype;
+
+%%
+piWrite(thisR);
+
+[obj,results] = piRender(thisR,...
+    'docker image name',thisDocker, ...
+    'render type',renderType);
+
+switch obj.type
+    case 'scene'
+        sceneWindow(obj);
+    case 'opticalimage'
+        oiWindow(obj);
+end
+
+end
