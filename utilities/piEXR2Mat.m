8cb8db36b1ea58d0da70df1c3b32080a181bbbec
David Cardinal <github@cardinalphoto.com>
2021-12-16 09:47:18 -0800

    resilient to stray files in renderings

--- utilities/piEXR2Mat.m
+++ utilities/piEXR2Mat.m
@@ -28,15 +28,33 @@ if status
     error('EXR to Binary conversion failed.')
 end
 filelist = dir([indir,sprintf('/%s_*',fname)]);
-nameparts = strsplit(filelist(1).name,'_');
-Nparts = numel(nameparts);
-height = str2double(nameparts{Nparts-2});
-width= str2double(nameparts{Nparts-1});
+
+% if there are both depth and radiance files
+% (on Windows at least) the Radiance files aren't
+% always listed first, so we need to find one to
+% get our base name
+baseName = '';
+dataFile = '';
+for ii = 1:numel(filelist)
+    if isequal(baseName, '') && ~isempty(strfind(filelist(ii).name, channelname))
+        dataFile = filelist(ii);
+        baseName = strsplit(filelist(ii).name,'.');
+        nameparts = strsplit(filelist(ii).name,'_');
+        Nparts = numel(nameparts);
+        height = str2double(nameparts{Nparts-2});
+        width= str2double(nameparts{Nparts-1});
+    end
+end
 
 if strcmp(channelname,'Radiance')
-    baseName = strsplit(filelist(1).name,'.');
+
     for ii = 1:31
         filename = fullfile(indir, [baseName{1},sprintf('.C%02d',ii)]);
+
+        % On windows suffix might not exist?
+        if ~isfile(filename)
+            filename = fullfile(indir, baseName{1});
+        end
         [fid, message] = fopen(filename, 'r');
         serializedImage = fread(fid, inf, 'float');
         data(:,:,ii) = reshape(serializedImage, height, width, 1);
@@ -44,7 +62,7 @@ if strcmp(channelname,'Radiance')
         delete(filename);
     end
 else
-        filename = fullfile(indir, filelist(1).name);
+        filename = fullfile(indir, baseName);
         [fid, message] = fopen(filename, 'r');
         serializedImage = fread(fid, inf, 'float');
         data = reshape(serializedImage, height, width, 1);
