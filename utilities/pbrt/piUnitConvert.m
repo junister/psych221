64d274660680df3218f7cf5ceca1b7f1c365f914
David Cardinal <github@cardinalphoto.com>
2021-12-28 10:55:30 -0800

    Add code to handle cell arrays

--- utilities/pbrt/piUnitConvert.m
+++ utilities/pbrt/piUnitConvert.m
@@ -1,17 +1,32 @@
 function thisR = piUnitConvert(thisR)
-    % scale camera position
-    thisR.lookAt.from = thisR.lookAt.from/100;
-    thisR.lookAt.to = thisR.lookAt.to/100;
-    
-    % scale objects
-    for ii = 2:numel(thisR.assets.Node)
-        thisNode = thisR.assets.Node{ii};
-        if strcmp(thisNode.type, 'branch')
-            % fix scale and translation
+% scale camera position
+thisR.lookAt.from = thisR.lookAt.from/100;
+thisR.lookAt.to = thisR.lookAt.to/100;
+
+% scale objects
+for ii = 2:numel(thisR.assets.Node)
+    thisNode = thisR.assets.Node{ii};
+    if strcmp(thisNode.type, 'branch')
+        % fix scale and translation
+        if isnumeric(thisNode.scale)
             thisNode.scale = thisNode.scale/100;
+        elseif iscell(thisNode.scale)
+            for j = 1:numel(thisNode.scale)
+                thisNode.scale{j} = thisNode.scale{j}/100;
+            end
+        else
+            warning("Not sure how to adjust scale");
+        end
+        if isnumeric(thisNode.translation)
             thisNode.translation = thisNode.translation/100;
-            
-            thisR.assets   = thisR.assets.set(ii, thisNode);
+        elseif iscell(thisNode.translation)
+            for j = 1:numel(thisNode.translation)
+                thisNode.translation{j} = thisNode.translation{j}/100;
+            end
+        else
+            warning("Not sure how to adjust translation");
         end
     end
+    thisR.assets   = thisR.assets.set(ii, thisNode);
+end
 end
