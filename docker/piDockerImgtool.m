ebcec663cebec76cd525cca2144b8d73dc80ffe4
David Cardinal <github@cardinalphoto.com>
2022-01-01 10:12:54 -0800

    Add Windows support

--- docker/piDockerImgtool.m
+++ docker/piDockerImgtool.m
@@ -72,6 +72,11 @@ dockerimage = p.Results.dockerimage;
 %% Switch on the cmds
 
 % Read the exr file and convert into the same directory
+if ~ispc
+    runDocker = 'docker run -ti ';
+else
+    runDocker = 'docker run -i ';
+end
 
 switch command
     case 'makeequiarea'
@@ -80,13 +85,16 @@ switch command
         [workdir, fname, ext] = fileparts(infile);
         fname = [fname,ext];
         
-        basecmd = 'docker run -ti --workdir=%s --volume="%s":"%s" %s %s';
+        basecmd = [runDocker ' --workdir=%s --volume="%s":"%s" %s %s'];
         
-        cmd = sprintf('imgtool makeequiarea %s --outfile equiarea-%s',fname,fname);
-        dockercmd = sprintf(basecmd, workdir, workdir, workdir, dockerimage, cmd);
+        cmd = sprintf('imgtool makeequiarea %s --outfile equiarea-%s', ...
+            dockerWrapper.pathToLinux(fname), dockerWrapper.pathToLinux(fname));
+        dockercmd = sprintf(basecmd, dockerWrapper.pathToLinux(workdir), ...
+            workdir, ...
+            dockerWrapper.pathToLinux(workdir), dockerimage, cmd);
     case 'help'
         % piDockerImgtool('help','help parameter','convert')
-        basecmd = 'docker run -ti %s %s';
+        basecmd = [runDocker ' %s %s'];
         if isempty(p.Results.helpparameter)
             cmd = sprintf('imgtool ');
         else
@@ -126,74 +134,3 @@ end
 
         
 end
-%{
-if ~ispc
-    % dockercmd = sprintf(basecmd, workdir, workdir, workdir, dockerimage, cmd);
-    [status,result] = system(dockercmd,'-echo');
-else
-    ourDocker = dockerWrapper();
-    ourDocker.command = ['imgtool convert --exr2bin ' channelname];
-    ourDocker.dockerImageName = dockerimage;
-    ourDocker.localVolumePath = indir;
-    ourDocker.targetVolumePath = indir;
-    ourDocker.inputFile = infile;
-    ourDocker.outputFile = ''; % imgtool uses a default
-    ourDocker.outputFilePrefix = '';
-    
-    [status, result] = ourDocker.runCommand();
-end
-
-if status
-    disp(result);
-    error('EXR to Binary conversion failed.')
-end
-filelist = dir([indir,sprintf('/%s_*',fname)]);
-
-% if there are both depth and radiance files
-% (on Windows at least) the Radiance files aren't
-% always listed first, so we need to find one to
-% get our base name
-baseName = '';
-dataFile = '';
-for ii = 1:numel(filelist)
-    if isequal(baseName, '') && ~isempty(strfind(filelist(ii).name, channelname))
-        dataFile = filelist(ii);
-        baseName = strsplit(filelist(ii).name,'.');
-        nameparts = strsplit(filelist(ii).name,'_');
-        Nparts = numel(nameparts);
-        height = str2double(nameparts{Nparts-2});
-        width= str2double(nameparts{Nparts-1});
-    end
-end
-
-if strcmp(channelname,'Radiance')
-
-    for ii = 1:31
-        filename = fullfile(indir, [baseName{1},sprintf('.C%02d',ii)]);
-
-        % On windows suffix might not exist?
-        if ~isfile(filename)
-            filename = fullfile(indir, baseName{1});
-        end
-        [fid, message] = fopen(filename, 'r');
-        serializedImage = fread(fid, inf, 'float');
-        try
-            data(:,:,ii) = reshape(serializedImage, height, width, 1);
-        catch
-            warning('Error reshaping radiance data.');
-            pause;
-        end
-        fclose(fid);
-        delete(filename);
-    end
-else
-        filename = fullfile(indir, baseName{1});
-        [fid, message] = fopen(filename, 'r');
-        serializedImage = fread(fid, inf, 'float');
-        data = reshape(serializedImage, height, width, 1);
-        fclose(fid);
-        delete(filename);
-end
-    
-end
-%}
