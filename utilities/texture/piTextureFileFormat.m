0789a16898305d06c73d9ca9b557414e6c55191d
Zhenyi Liu <zhenyi27@stanford.edu>
2022-01-17 11:54:07 +0800

    Convert RGB image to alpha image

--- utilities/texture/piTextureFileFormat.m
+++ utilities/texture/piTextureFileFormat.m
@@ -1,18 +1,18 @@
 function thisR = piTextureFileFormat(thisR)
 % Convert textures to PNG format.
 %     thisR = piTextureFileFormat(thisR)
-% 
+%
 % Brief description:
 %   We convert any texture files used in the scene to PNG format.
-% 
-% Inputs: 
+%
+% Inputs:
 %   thisR: render recipe.
 %
 % Outputs:
 %   thisR: render recipe with updated textures.
 %
 %
-% Note (Zhenyi): There is a weird case for me, when I use a JPG texture, the 
+% Note (Zhenyi): There is a weird case for me, when I use a JPG texture, the
 %                PBRT runs without error, however the surface reflection
 %                which used a JPG texture is missing.
 %
@@ -23,37 +23,50 @@ textureList = values(thisR.textures.list);
 inputDir = thisR.get('input dir');
 
 for ii = 1:numel(textureList)
-
+    
     if ~isfield(textureList{ii},'filename')
         continue;
     end
     [path, name, ext] = fileparts(textureList{ii}.filename.value);
-    
+    texSlotName = textureList{ii}.filename.value;
+    thisImgPath = fullfile(inputDir, texSlotName);
+
     if isempty(find(strcmp(ext, {'.png','.PNG','.exr'}),1))
-        
-        texSlotName = textureList{ii}.filename.value;
-        thisImgPath = fullfile(inputDir, texSlotName);
-        
         if exist(thisImgPath, 'file')
- 
+            
             outputPath = fullfile(inputDir, path, [name,'.png']);
             
             if ~exist(outputPath,'file')
                 thisImg = imread(thisImgPath);
                 imwrite(thisImg,outputPath);
             end
-      
+            
             % update texture slot
             textureList{ii}.filename.value = fullfile(path, [name,'.png']);
             thisR.textures.list(textureList{ii}.name) = textureList{ii};
-                
+            
             fprintf('Texture: %s is converted \n',textureList{ii}.filename.value);
             
             % remove the original jpg textures.
-%             delete(thisImgPath);
+            %             delete(thisImgPath);
         else
             warning('Texture: %s is missing',textureList{ii}.filename.value);
         end
+    end
+    
+    % convert RGB to alpha map
+    if contains(textureList{ii}.name,{'tex_','.alphamap.'}) && ...
+            exist(fullfile(inputDir, texSlotName),'file')
+        
+        outputPath = fullfile(inputDir, path, [name,'_alphamap.png']);
+        thisImg = imread(thisImgPath);
+        thisImg = thisImg(:,:,1);
+        thisImg(thisImg~=0)=255;
+        imwrite(thisImg,outputPath);
+        textureList{ii}.filename.value = fullfile(path, [name,'_alphamap.png']);
+        thisR.textures.list(textureList{ii}.name) = textureList{ii};
+        
+        fprintf('Texture: %s is converted \n',textureList{ii}.filename.value);
     end
 end
 
@@ -91,7 +104,7 @@ for ii = 1:numel(matKeys)
         fprintf('Normal Map: %s is converted \n',normalImgPath);
         
         % remove the original jpg textures.
-%         delete(thisImgPath);
+        %         delete(thisImgPath);
     else
         warning('Normal Map: %s is missing',normalImgPath);
     end
