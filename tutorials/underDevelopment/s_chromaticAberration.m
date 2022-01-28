bcb2eb45b39a42e708b6026327c64fd08459e647
David Cardinal <github@cardinalphoto.com>
2022-01-28 13:38:58 -0800

    switch to slanted bar

--- tutorials/underDevelopment/s_chromaticAberration.m
+++ tutorials/underDevelopment/s_chromaticAberration.m
@@ -1,9 +1,10 @@
 %% s_chromaticAberrationv3.m
 %
 % Demonstrate the chromatic aberration present in lens rendering. Adapted
-% from s_texturedPlane.m
+% from a sample scene
 %
 % TL SCIEN Team, 2018
+% v4 version D.Cardinal 2022
 
 %% Initialize ISET and Docker
 
@@ -12,38 +13,26 @@ if ~piDockerExists, piDockerConfig; end
 
 %% Read the pbrt scene
 % Read the main scene pbrt file.  Return it as a recipe
+% used to use textured plane, but it's not in v4 (yet?)
 useScene = 'cornell_box';
 thisR = piRecipeDefault('scene name', useScene);
 % add a light
 pointLight = piLightCreate('point','type','point','cameracoordinate', true);
 thisR.set('light',pointLight, 'add');
 
-%% Scale and translate the plane
-scale = 0.5; % 1*0.5 = 0.5 m
-translate = 1; % m
+%% Attach a desired texture to part of the scene
+ourAsset = '001_large_box_O';
+piMaterialsInsert(thisR);
+piAssetTranslate(thisR,ourAsset,[.15 .11 0]);
+thisR.set('asset',ourAsset,'material name','slantededge');
 
-% The (textured) plane has specifically been named "Plane" in this scene. We
-% also know that our camera is located at the origin and looking down the
-% positive y-axis. 
-% Note: The order of scaling and translating matters!
-%piAssetScale(thisR,'Checkerboard_B',[scale scale scale]); % The plane is oriented in the x-z plane
-%piAssetTranslate(thisR,'Checkerboard_B',[0 0 translate]); 
-
-%% Attach a desired texture
-%imageName = 'squareResolutionChart.exr';
-%imageFile = fullfile(piRootPath,'data','imageTextures',imageName);
-
-%copyfile(imageFile,workingDir);
-%thisR = piWorldFindAndReplace(thisR,'dummyTexture.exr',imageName);
-
-%% Attach a lens
-
+%% Attach a camera with a lens
 thisR.set('camera',piCameraCreate('omni'));     % Has a lens
-thisR.set('aperture',2);             % mm
-thisR.set('film resolution',128);    % Spatial samples
+thisR.set('aperture',7);             % mm
+thisR.set('film resolution',512);    % Spatial samples
 thisR.set('rays per pixel',128);     % Rendering samples
-thisR.set('film diagonal', 10);            % Size of film in mm
-thisR.set('focusdistance',1)
+thisR.set('film diagonal', 2);            % Size of film in mm
+thisR.set('focusdistance',1.6); % to the large box
 
 fprintf('Rendering with lens:   %s\n',thisR.get('lens file'));
 
@@ -51,16 +40,15 @@ fprintf('Rendering with lens:   %s\n',thisR.get('lens file'));
 %% Turn on chromatic aberration and render
 
 % This takes longer because we are using more wavelength samples to
-% trace through the lens (8 bands).
+% trace through the lens (8 bands by default).
 thisR.set('chromatic aberration',true);
 piWrite(thisR);
 
 [oiCA, results] = piRender(thisR,'render type','radiance');
-oiCA = oiSet(oiCA,'name','CA');
+oiCA = oiSet(oiCA,'name','CA 8 bands');
 oiWindow(oiCA);
 
 %% Render without chromatic aberration
-
 thisR.set('chromatic aberration',false);
 
 % the old version did a file copy here, but
@@ -73,12 +61,12 @@ oi = oiSet(oi,'name','noCA');
 % Show it in ISET
 oiWindow(oi);  
 
-%% Now with only 4 bands
+%% Now with 15 bands
 thisR.set('chromatic aberration',15);
 piWrite(thisR);
 
 [oiCA, results] = piRender(thisR,'render type','radiance');
-oiCA = oiSet(oiCA,'name','CA');
+oiCA = oiSet(oiCA,'name','CA 15 bands');
 oiWindow(oiCA);
 
 %% End
