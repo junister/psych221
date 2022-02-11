2c5b4f6f71c41a2b524461c260f65f45f564c90e
Brian Wandell <wandell@stanford.edu>
2022-02-10 17:29:47 -0800

    Create piDockerRemoteInit.m
    Simplify setting pref for docker renderString.  Much left to do.

---
+++ docker/piDockerRemoteInit.m
@@ -0,0 +1,32 @@
+function renderString = piDockerRemoteInit(varargin)
+%Initialize the docker preference for remote executation
+%
+% These are the key/value options that we pass in piRender when
+% running with the GPU
+%
+% This persists across Matlab sessions.  So you don't need to run it
+% every time.
+%
+
+%% Example
+%{
+  s = piDockerRemoteInit;
+  s{8}
+%}
+p = inputPar        
+renderString = {'gpuRendering', true, ...
+    'remoteMachine', 'muxreconrt.stanford.edu',...
+    'renderContext', 'wandell-v4',...
+    'remoteImage',  'digitalprodev/pbrt-v4-gpu-ampere-mux-shared', ...
+    'remoteRoot','/home/wandell', ...
+    'remoteUser', 'wandell', ...
+    'whichGPU', 0};
+
+% 'localRoot', <for WSL>, ...   % Not needed on MacOS
+
+setpref('docker', 'renderString', renderString);
+getpref('docker', 'renderString')   % Check
+
+setpref('docker','verbosity', 1)
+
+end
