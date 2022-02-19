01570b5579aff307870a165a2461dfb57238d9a9
David Cardinal <github@cardinalphoto.com>
2022-02-18 16:44:50 -0800

    Fix render pause & asset validation output

--- validation/v_Assets.m
+++ validation/v_Assets.m
@@ -4,25 +4,35 @@ ieInit;
 parentRecipe = piRecipeDefault('scene name','cornell_box');
 lightName = 'from camera';
 ourLight = piLightCreate(lightName,...
-                        'type','distant',...
-                        'cameracoordinate', true);
+    'type','distant',...
+    'cameracoordinate', true);
 recipeSet(parentRecipe,'lights', ourLight,'add');
 piWRS(parentRecipe);
 
 assetFiles = dir([fullfile(piRootPath,'data','assets'),filesep(),'*.mat']);
 report = '';
 for ii = 1:numel(assetFiles)
+
+    % I think we need to reload to avoid issues
+    % from previous runs
+    parentRecipe = piRecipeDefault('scene name','cornell_box');
+    lightName = 'from camera';
+    ourLight = piLightCreate(lightName,...
+        'type','distant',...
+        'cameracoordinate', true);
+    recipeSet(parentRecipe,'lights', ourLight,'add');
     assetName = assetFiles(ii).name;
     try
         ourAsset = piAssetLoad(assetName);
         combinedR = piRecipeMerge(parentRecipe, ourAsset.thisR);
         piWRS(combinedR);
-        report = [report sprintf("Asset: %s Succeeded.\n", assetName)]; %#ok<AGROW> 
+        report = [report sprintf("Asset: %s Succeeded.\n", assetName)]; %#ok<AGROW>
     catch
-        dockerWrapper.cleanup(); 
-        report = [report sprintf("Asset: %s failed", assetName)]; %#ok<AGROW> 
+        % dockerWrapper.reset;
+        report = [report sprintf("Asset: %s failed \n", assetName)]; %#ok<AGROW>
     end
 
-    fprintf("Asset validation results: \n %s", report);
 end
+fprintf("Asset Validation Results: \n");
+fprintf("%s", report);
 
