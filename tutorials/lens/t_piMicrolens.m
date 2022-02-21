56daec78cbf0a5fbba03ef5b72bffeec0e1e48da
David Cardinal <github@cardinalphoto.com>
2022-02-21 13:56:08 -0800

    Update to current docker img

--- tutorials/lens/t_piMicrolens.m
+++ tutorials/lens/t_piMicrolens.m
@@ -31,7 +31,7 @@ if ispc
 else
     dFlags = '-ti';
 end 
-status = system(['docker run ' dFlags '--platform linux/amd64 --rm digitalprodev/pbrt-v4-cpu-lenstool lenstool']);
+status = system(['docker run ' dFlags ' --rm ' dockerWrapper.localImage ' lenstool']);
 
 %% Shows the lenses
 
