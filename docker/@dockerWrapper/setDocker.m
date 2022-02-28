e867187aa94d72c16c851802c9239a5f81204a2c
David Cardinal <github@cardinalphoto.com>
2022-02-28 15:34:50 -0800

    Add ability to set docker parameters

---
+++ docker/@dockerWrapper/setDocker.m
@@ -0,0 +1,40 @@
+function  setDocker(options)
+%SETDocker set Docker rendering options
+
+arguments
+    options.whichGPU {mustBeNumeric} = 0;
+    options.gpuRendering;
+
+    % these relate to remote/server rendering
+    % they overlap while we learn the best way to organize them
+    options.remoteMachine = ''; % for syncing the data
+    options.remoteUser = ''; % use for rsync & ssh/docker
+
+    options.remoteImage = '';
+    options.remoteImageTag = '';
+    options.remoteRoot = ''; % we need to know where to map on the remote system
+    options.localRoot = ''; % for the Windows/wsl case (sigh)
+
+end
+setpref('docker','whichGPU', options.whichGPU);
+
+options.gpuRendering;
+if ~isempty(options.remoteUser)
+    setpref('docker', 'remoteUser', options.remoteUser);
+end
+if ~isempty(options.remoteImage)
+    setpref('docker', 'remoteImage', options.remoteImage);
+end
+if ~isempty(options.remoteImageTag)
+    setpref('docker', 'remoteImageTag', options.remoteImageTag);
+end
+if ~isempty(options.remoteRoot)
+    setpref('docker', 'remoteRoot', options.remoteRoot);
+end
+if ~isempty(options.localRoot)
+    setpref('docker', 'localRoot', options.localRoot);
+end
+
+dockerWrapper.reset;
+end
+
--- docker/@dockerWrapper/setGPU.m
+++
@@ -1,9 +0,0 @@
-function  setGPU(options)
-%SETGPU set GPU device & options
-    arguments
-        options.whichGPU {mustBeNumeric} = 0;
-    end
-    setpref('docker','whichGPU', options.whichGPU);
-    dockerWrapper.reset;
-end
-
