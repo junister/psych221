7d2c57aa0b97e95a77c17159f598a64ddd3ce1b9
David Cardinal <github@cardinalphoto.com>
2022-02-28 16:01:12 -0800

    better comments

--- docker/@dockerWrapper/setParams.m
+++ docker/@dockerWrapper/setParams.m
@@ -1,5 +1,24 @@
 function  setParams(options)
-%SETDocker set Docker rendering options
+%%
+% SETParams: Optionally set Docker rendering options
+%
+% Underlying mechanism is setpref(), getpref() so changes are 
+% persistent across Matlab sessions.
+%
+%   dockerWrapper.setParams('docker','remoteUser',<remoteUser>);
+%   dockerWrapper.setParams('docker','remoteRoot',<remoteRoot>); % where we will put the iset tree
+%   dockerWrapper.setParams('docker','localRoot',<localRoot>); % only needed for WSL if not \mnt\c
+%
+% Other options you can specify:
+% If you need to turn-off GPU Rendering set to false
+%  dockerWrapper.setParams('docker','gpuRendering',false);
+%
+% If you are having issues with :latest, you can go back to :stable
+% dockerWrapper.setParams('docker', 'remoteImageTag','stable');
+%
+% Change which gpu to use on a server
+% dockerWrapper.setParams('docker', 'whichGPU', <#>); % default is 0
+%
 
 arguments
     options.whichGPU {mustBeNumeric} = 0;
