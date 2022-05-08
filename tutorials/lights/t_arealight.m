b138c39cfd0642d1f69a9ab176d981d656cc4101
Brian Wandell <wandell@stanford.edu>
2022-05-08 12:58:34 -0700

    comments, tiny changes.

--- tutorials/lights/t_arealight.m
+++ tutorials/lights/t_arealight.m
@@ -1,13 +1,18 @@
 %% Adjust the spread angle of area lights
-ieInit;
-piDockerConfig;
+
 %%
+ieInit;
+if ~piDockerExists, piDockerConfig; end
+
+%% 
 fileName = fullfile(piRootPath, 'data/V4','arealight','arealight.pbrt');
 thisR    = piRead(fileName);
+
+%%
 piWRS(thisR,'gamma',0.5);
 
 %%
-piLightList(thisR);
+thisR.show('lights');
 thisR.set('light','AreaLightRectangle_L','spread val',20);
 thisR.set('light','AreaLightRectangle.001_L','spread val',20);
 thisR.set('light','AreaLightRectangle.002_L','spread val',20);
