cbe2fc0018af11cfa72c85c5739e45dab743680b
Brian Wandell <wandell@stanford.edu>
2022-05-13 18:02:12 -0700

    Putting in Krithin assets

---
+++ data/assets/s_assetsKrithin.m
@@ -0,0 +1,31 @@
+%% Test Krithin's table
+
+sceneDir = 'table';
+sceneFile = 'Table-pbrt.pbrt';
+exporter = 'PARSE';
+
+FilePath = fullfile(piRootPath,'data','V4',sceneDir);
+fname = fullfile(FilePath,sceneFile);
+exist(fname,'file')
+
+thisR = piRead(fname, 'exporter', exporter);
+
+thisR.set('skymap','room.exr');
+
+scene = piWRS(thisR);
+
+%% Try the Arch
+
+sceneDir = 'arch';
+sceneFile = 'Arch3d-pbrt.pbrt';
+exporter = 'PARSE';
+
+FilePath = fullfile(piRootPath,'data','V4',sceneDir);
+fname = fullfile(FilePath,sceneFile);
+exist(fname,'file')
+
+thisR = piRead(fname, 'exporter', exporter);
+
+thisR.set('skymap','room.exr');
+
+scene = piWRS(thisR);
