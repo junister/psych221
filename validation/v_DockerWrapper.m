e745d6aea131a2fd3c44fade0564c29ac4756c33
Brian Wandell <wandell@stanford.edu>
2022-05-17 21:16:32 -0700

    Update v_DockerWrapper.m
    Adjusted try/catch and added more comments for reading.  Reminder to set renderContex in header comments.  But maybe we should deal with that in the local cpu set up?

--- validation/v_DockerWrapper.m
+++ validation/v_DockerWrapper.m
@@ -1,39 +1,86 @@
-function success = v_DockerWrapper()
-% Validate our docker Wrapper
-
-% Assumes that prefs are set for
-% 
-% remoteMachine
-% remoteImage (if on an unknown server)
+% v_DockerWrapper()
 %
+% Validate various docker Wrapper calls
+%
+% Assumes that 'docker' prefs are set for
+% 
+%    Variables            Stanford Defaults
+%   ---------------       -----------------
+%   remoteMachine         muxreconrt.stanford.edu
+%   remoteImage           digitalprodev/XXX
+%   renderContext         remote-mux
+%
+
+%% Initialize as usual
+ieInit();
+if ~piDockerExists, piDockerConfig; end
+
+%% Default rendering
+
+fprintf('*** User defaults ... ****\n');
+fprintf('-------------------------------------\n\n')
+
 try
     % Simplest case piWRS/piRender create a docker wrapper as needed
     % First, clear out any that exist
-    ieInit();
     dockerWrapper.reset();
     sampleScene = piRecipeDefault('scene name','chess set');
     piWRS(sampleScene);
 
-    fprintf('Simple piWRS succeeded.\n');
+    fprintf('succeeded.\n');
+    fprintf('-------------------------------------\n')
+catch
+    fprintf('failed.\n');
+    fprintf('-------------------------------------\n')
+end
 
+%% Default rendering, verbose
+fprintf('*** User default verbose ... ***\n');
+fprintf('-------------------------------------\n\n')
+
+try
     dockerWrapper.reset;
     ourDocker = dockerWrapper('verbosity',2);
     piWRS(sampleScene, 'our docker', ourDocker);
     
-    fprintf('Docker wrapper Default Case succeeded\n');
+    fprintf('succeeded\n');
+    fprintf('-------------------------------------\n')
+catch
+    fprintf('failed\n');
+    fprintf('-------------------------------------\n')
 
+end
+
+%% Local rendering CPU
+
+fprintf('*** Local render on CPU ... ***');
+fprintf('-------------------------------------\n\n')
+
+try
     % test local rendering on CPU
     % need to turn off GPU rendering or we've given it conflicting
     % instructions in the case where the GPU is remote
     ourDocker = dockerWrapper('localRender',true,'gpuRendering', false,'verbosity',2);
     piWRS(sampleScene,'our docker', ourDocker);
 
-    fprintf('Docker wrapper Local Case succeeded\n');
+    fprintf('succeeded\n');
+    fprintf('-------------------------------------\n')
 
+catch
+    fprintf(' failed\n');
+    fprintf('-------------------------------------\n')
+end
+
+%% Remote rendering (mux) CPU
+
+fprintf('*** Remote rendering on CPU ... ***\n');
+fprintf('-------------------------------------\n\n')
+try
     % test remote CPU rendering
     % currenctly we keep a cache of 1 GPU and 1 CPU container
     % so creating a second one of either requires a reset
     dockerWrapper.reset();
+
     % NOTE: To render remotely, we may need to specify the .remoteImage
     %       since we don't know what CPU it is
     x86Image = 'digitalprodev/pbrt-v4-cpu:latest';
@@ -41,11 +88,11 @@ try
         'remoteImage',x86Image, 'verbosity',2);
     piWRS(sampleScene,'our docker', ourDocker);
 
-    fprintf('Docker wrapper CPU Case succeeded\n');
-    disp('Docker wrapper validation succeeded\n');
-    success = 0;
+    fprintf('succeeded\n');
+    fprintf('-------------------------------------\n')
 catch
-    warning("Docker Wrapper validation failed.\n");
-    success = -1;
+    fprintf(' failed\n');
+    fprintf('-------------------------------------\n')
 end
-end
+
+%% END
