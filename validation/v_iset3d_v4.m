e74be3f3b1e5cf08f40e8f1008a3ca91500cbda5
David Cardinal <github@cardinalphoto.com>
2022-05-21 16:38:36 -0700

    Add v_DockerWrapper, remove duplicate camera test. [Doesn't fix the issue we're having with materials, but shouldn't make things worse:)]

--- validation/v_iset3d_v4.m
+++ validation/v_iset3d_v4.m
@@ -13,11 +13,22 @@ setpref('ISET3d', 'tStart', tic);
 
 %% Basic
 
+%% Quick version of DockerWrapper tests
+disp('*** DOCKER -- v_DockerWrapper');
+setpref('ISET3d', 'tvdockerStart', tic);
+try
+    v_DockerWrapper('length','short');
+    setpref('ISET3d', 'tvdockerTime', toc(getpref('ISET3d', 'tvdockerStart', 0)));
+catch
+    disp('Docker Wrapper test failed');
+    disp('Make sure you have a remote image set up before running');
+    setpref('ISET3d', 'tvdockerTime', -1);
+end
+
 %% Depth in x,y,z dimensions
 disp('*** DEPTH -- t_piIntro_macbeth')
 setpref('ISET3d', 'tvdepthStart', tic);
 try
-    % seems to have broken?
     t_piIntro_macbeth;               % Gets the depth map
     setpref('ISET3d', 'tvdepthTime', toc(getpref('ISET3d', 'tvdepthStart', 0)));
 catch
@@ -28,7 +39,6 @@ end
 %% Omni camera (e.g. one with a lens)
 disp('v_omni')
 setpref('ISET3d', 'tvomniStart', tic);
-disp('Run omni once fixed');
 v_omni;          
 setpref('ISET3d', 'tvomniTime', toc(getpref('ISET3d', 'tvomniStart', 0)));
 
@@ -44,6 +54,7 @@ setpref('ISET3d', 'tvmaterialStart', tic);
 t_piIntro_material;
 setpref('ISET3d', 'tvmaterialTime', toc(getpref('ISET3d', 'tvmaterialStart', 0)));
 
+%% Demo working with lights
 disp('*** LIGHTS -- t_piIntro_light')
 setpref('ISET3d', 'tvlightStart', tic);
 try
@@ -54,32 +65,19 @@ catch
     setpref('ISET3d', 'tvlightTime', -1);
 end
 
+%% Our Intro Demo
 disp('*** INTRO -- t_piIntro')
 setpref('ISET3d', 'tvpbrtStart', tic);
 t_piIntro;
 setpref('ISET3d', 'tvpbrtTime', toc(getpref('ISET3d', 'tvpbrtStart', 0)));
 
-%%  Check that the scenes in the data directory still run
-%{
-disp('v_piDataScenes')
-v_piDataScenes;                  % Checks the local data scenes
-%}
-%%  Rotate the camera
-
-disp('*** CAMERA MOTION -- t_cameraMotion')
-setpref('ISET3d', 'tvcammotionStart', tic);
-t_cameraMotion;
-setpref('ISET3d', 'tvcammotionTime', toc(getpref('ISET3d', 'tvcammotionStart', 0)));
-
-%% Maybe redundant with prior cameramotion
-
+%%  Translate and Rotate the camera
 disp('*** CAMERA POSITION -- t_cameraPosition')
 setpref('ISET3d', 'tvcampositionStart', tic);
 t_cameraPosition;
 setpref('ISET3d', 'tvcampositionTime', toc(getpref('ISET3d', 'tvcampositionStart', 0)));
 
 %% Various renders of the Chess Set
-
 disp('*** CHESS SET -- t_piIntro_chess')
 setpref('ISET3d', 'tvchessStart', tic);
 try
@@ -90,7 +88,7 @@ end
 setpref('ISET3d', 'tvchessTime', toc(getpref('ISET3d', 'tvchessStart', 0)));
 
 
-%% It runs, but we are not happy
+%% This does not run in v4 yet
 %{
 disp('t_piIntro_meshLabel')
 t_piIntro_meshLabel
@@ -103,7 +101,7 @@ v_skymap;
 setpref('ISET3d', 'tvskymapTime', toc(getpref('ISET3d', 'tvskymapStart', 0)));
 
 %% Textures
-
+% THIS DOES NOT WORK IN v4 yet
 %{
 disp('*** TEXTURES -- t_piIntro_texture')
 t_piIntro_texture;
@@ -117,13 +115,12 @@ disp(strcat("v_ISET3d-v4 (LOCAL) ran  on: ", glData.Vendor, " ", glData.Renderer
 disp(strcat("v_ISET3d-v4 (LOCAL) ran  in: ", string(afterTime - beforeTime), " seconds of CPU time."));
 disp(strcat("v_ISET3d-v4 ran  in: ", string(tTotal), " total seconds."));
 disp('===========');
+fprintf("Docker:     %5.1f seconds.\n", getpref('ISET3d','tvdockerTime'));
 fprintf("Depth:      %5.1f seconds.\n", getpref('ISET3d','tvdepthTime'));
 fprintf("Omni:       %5.1f seconds.\n", getpref('ISET3d','tvomniTime'));
 fprintf("Assets:     %5.1f seconds.\n", getpref('ISET3d','tvassetsTime'));
 fprintf("Material:   %5.1f seconds.\n", getpref('ISET3d','tvmaterialTime'));
 fprintf("Light:      %5.1f seconds.\n", getpref('ISET3d','tvlightTime'));
-% fprintf("PBRT:       %5.1f seconds.\n", getpref('ISET3d','tvpbrtTime'));
-fprintf("Cam Motion: %5.1f seconds.\n", getpref('ISET3d','tvcammotionTime'));
 fprintf("Cam Pos.:   %5.1f seconds.\n", getpref('ISET3d','tvcampositionTime'));
 fprintf("Chess Set:  %5.1f seconds.\n", getpref('ISET3d','tvchessTime'));
 fprintf("Skymap:     %5.1f seconds.\n", getpref('ISET3d','tvskymapTime'));
