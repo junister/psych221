484aa9a9f8850d7b39cd35e01ea0ef7caf486a10
Brian Wandell <wandell@stanford.edu>
2022-07-04 16:44:14 -0700

    Identifying problems with instance creation
    See notes (TODO) in piObjectInstanceCreate.
    
    Also, added many comments to 'Remove' form.

--- tutorials/scene/t_piSceneInstances.m
+++ tutorials/scene/t_piSceneInstances.m
@@ -15,6 +15,16 @@
 ieInit;
 if ~piDockerExists, piDockerConfig; end
 
-%% 
-thisR = piRecipeDefault('chess set');
+%% Render the basic scene
 
+thisR = piRecipeDefault('scene name','simple scene');
+scene = piWRS(thisR);
+
+%% Create a second instance if the yellow guy
+
+thisR.get('asset names')
+
+yellowM = '001_figure_6m_O';
+yellowPos = thisR.get('asset',yellowM,'world position');
+yellowB = '0022ID_figure_6m_B';
+thisR = piObjectInstanceCreate(thisR,yellowB,'position',yellowPos + [0.5 0.5 0.5]);
