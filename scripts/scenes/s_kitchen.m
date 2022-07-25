716b1d30b0ed822c39ae00e4df717670c5d9c2eb
Brian Wandell <wandell@stanford.edu>
2022-07-25 10:23:40 -0700

    Object distance was the important issue.
    Added kitchen script.  Not sure about the object distance and scaling.  Black image when the object distance was as in bistro.

---
+++ scripts/scenes/s_kitchen.m
@@ -0,0 +1,46 @@
+%% s_bistro
+%
+% Worked with cardinal download on July 11, 2022
+%
+%
+
+%%
+ieInit;
+if ~piDockerExists, piDockerConfig; end
+
+%%
+
+resolution = [640 640]*0.5;
+
+thisR = piRecipeDefault('scene name','kitchen');
+thisR.set('rays per pixel',512);
+thisR.set('film resolution',resolution);
+thisR.set('render type',{'radiance','depth'});
+
+%% This renders the scene
+
+scene = piWRS(thisR);
+
+%%  You can see the depth from the depth map.
+% scenePlot(scene,'depth map');
+
+%% Another double Gauss
+
+% lensList
+lensfile  = 'dgauss.22deg.3.0mm.json';    % 30 38 18 10
+thisR.camera = piCameraCreate('omni','lensFile',lensfile);
+
+thisR.set('film diagonal',5);  %% 33 mm is small
+thisR.set('object distance',2);  % Move closer. The distance scaling is weird.
+piWRS(thisR,'name','DG 10m');
+
+%% Fisheye
+
+lensfile = 'fisheye.87deg.3.0mm.json';
+thisR.camera = piCameraCreate('omni','lensFile',lensfile);
+oi = piWRS(thisR,'name','fisheye 10m');
+oi = piAIdenoise(oi);
+oiWindow(oi);
+
+%%
+
