1130758803847fea0daf381200b8bf16fd329be7
Brian Wandell <wandell@stanford.edu>
2022-07-30 10:29:45 -0700

    Update piRecipeDefault.m

--- utilities/recipe/piRecipeDefault.m
+++ utilities/recipe/piRecipeDefault.m
@@ -432,7 +432,7 @@ function fname = piSceneWebTest(sceneName,sceneFile)
 % Check for a web scene
 
 % See if the scene is already in data/scene/web
-FilePath = fullfile(piRootPath,'data','scene','web',sceneName);
+FilePath = fullfile(piRootPath,'data','scenes','web',sceneName);
 fname = fullfile(FilePath,sceneFile);
 
 % Download the file to data/scene/web
