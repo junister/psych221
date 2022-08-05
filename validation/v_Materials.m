5562511e5994ed1322315e21f969bf8f6e59de1f
David Cardinal <github@cardinalphoto.com>
2022-08-05 09:04:35 -0700

    sample code

--- validation/v_Materials.m
+++ validation/v_Materials.m
@@ -1,1 +1,15 @@
 % Need to add a validation script that runs through all materials!
+
+% Starting code from the MaterialInsert test:
+thisR = piRecipeDefault('scene name','bunny');
+
+thisR.set('skymap','room.exr');
+thisR.set('asset','001_Bunny_O','scale',4);
+thisR.set('nbounces',3);
+
+%%
+piMaterialsInsert(thisR,'names','glossy-red');
+
+thisR.set('asset','001_Bunny_O','material name','glossy-red');
+piWRS(thisR);
+
