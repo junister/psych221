0a4a267fb79bdd9d9f70495dff4c01969b42ecc1
David Cardinal <github@cardinalphoto.com>
2022-08-05 10:40:32 -0700

    Validate which materials can be used

--- validation/v_Materials.m
+++ validation/v_Materials.m
@@ -1,15 +1,72 @@
 % Need to add a validation script that runs through all materials!
 
 % Starting code from the MaterialInsert test:
+ieInit;
 thisR = piRecipeDefault('scene name','bunny');
-
 thisR.set('skymap','room.exr');
-thisR.set('asset','001_Bunny_O','scale',4);
+thisR.set('asset','Bunny_O','scale',4);
 thisR.set('nbounces',3);
 
 %%
-piMaterialsInsert(thisR,'names','glossy-red');
+results = [];
+allMaterials = piMaterialPresets('list');
+for ii = 1:numel(allMaterials)
+    try
+        % we need to re-load so a broken material
+        % doesn't cause us to error out
+        thisR = piRecipeDefault('scene name','bunny');
+        thisR.set('skymap','room.exr');
+        thisR.set('asset','Bunny_O','scale',4);
+        thisR.set('nbounces',3);
+        piMaterialsInsert(thisR,'names',allMaterials{ii});
+        thisR.set('asset','Bunny_O','material name',allMaterials{ii});
+        piWRS(thisR);
+        results = [results sprintf("Material: %s Succeeded\n",allMaterials{ii})];
+    catch EX
+        results = [results sprintf("Material: %s FAILED: %s\n",allMaterials{ii},EX.message)];
+    end
+end
 
-thisR.set('asset','001_Bunny_O','material name','glossy-red');
-piWRS(thisR);
-
+for ii = 1:numel(results)   
+    fprintf(results{ii});
+end
+%{ 
+results as of Aug 5, 2022:
+Material: diffuse-gray Succeeded
+Material: diffuse-red Succeeded
+Material: diffuse-white Succeeded
+Material: glossy-black Succeeded
+Material: glossy-gray Succeeded
+Material: glossy-red Succeeded
+Material: glossy-white Succeeded
+Material: glass Succeeded
+Material: red-glass FAILED: Render failed.
+Material: glass-bk7 Succeeded
+Material: glass-baf10 Succeeded
+Material: glass-fk51a Succeeded
+Material: glass-lasf9 Succeeded
+Material: glass-f5 Succeeded
+Material: glass-f10 Succeeded
+Material: glass-f11 Succeeded
+Material: mirror Succeeded
+Material: metal-ag Succeeded
+Material: chrome Succeeded
+Material: rough-metal Succeeded
+Material: metal-au Succeeded
+Material: metal-cu Succeeded
+Material: metal-cuzn Succeeded
+Material: metal-mgo Succeeded
+Material: metal-tio2 Succeeded
+Material: tire Succeeded
+Material: marble-beige Succeeded
+Material: tiles-marble-sagegreen-brick Succeeded
+Material: checkerboard Succeeded
+Material: ringsrays Succeeded
+Material: macbethchart Succeeded
+Material: slantededge Succeeded
+Material: dots Succeeded
+Material: wood-floor-merbau Succeeded
+Material: wood-medium-knots Succeeded
+Material: wood-light-large-grain Succeeded
+Material: wood-mahogany Succeeded
+%}
