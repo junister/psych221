c065d0c42200e8f669f55174b9437814773b626d
Brian Wandell <wandell@stanford.edu>
2022-08-09 15:56:18 -0700

    Simplifying and testing

--- tutorials/lights/t_arealight.m
+++ tutorials/lights/t_arealight.m
@@ -22,38 +22,38 @@ if ~piDockerExists, piDockerConfig; end
 fileName = fullfile(piRootPath, 'data','scenes','arealight','arealight.pbrt');
 thisR    = piRead(fileName);
 
-%% Default properties
-scene = piWRS(thisR,'render flag','hdr');
-
-%% Show the lights in the file and rename them for convenience
-thisR.show('lights');
-
 % The no number is the blue one
 % The 002 light is the green one.
 % The 001 is the red one
 % the 003 must be the yellow one.
 
-%%
-% TODO: This sets the name of the light asset.  It must always have a _L if
-% it is a light. There is also a name in the 'lght{1}' slot. That should
-% probably be set to align with this name.
+%% This sets the name of the light asset.  
+%
+% The name must always have a _L if it is a light. There is also a
+% name in the 'lght{1}' slot. That should probably be set to align
+% with this name.
 thisR.set('asset','AreaLightRectangle_L','name','Area_Blue_L');
 thisR.set('asset','AreaLightRectangle.001_L','name','Area_Red_L');
 thisR.set('asset','AreaLightRectangle.002_L','name','Area_Green_L');
 thisR.set('asset','AreaLightRectangle.003_L','name','Area_Yellow_L');
-
 thisR.show('lights');
 
+scene = piWRS(thisR,'render flag','hdr');
+
 %% Plot the luminance across a line
+
 roiLocs = [1 74];
 sz = sceneGet(scene,'size');
 scenePlot(scene,'luminance hline',roiLocs);
 ieROIDraw(scene,'shape','line','shape data',[1 sz(2) roiLocs(2) roiLocs(2)]);
 
 %% The green light is bright.  Let's reduce its intensity.
+gScale = thisR.get('light','Area_Green_L','specscale');
 
-thisR.set('light','Area_Green_L','specscale',40);
+thisR.set('light','Area_Green_L','specscale',gScale/4);
 scene = piWRS(thisR,'render flag','hdr');
+
+%%
 roiLocs = [1 74];
 sz = sceneGet(scene,'size');
 scenePlot(scene,'luminance hline',roiLocs);
@@ -76,30 +76,38 @@ sz = sceneGet(scene,'size');
 scenePlot(scene,'luminance hline',roiLocs);
 ieROIDraw(scene,'shape','line','shape data',[1 sz(2) roiLocs(2) roiLocs(2)]);
 
-%%
+roiRect = [416 124 34 42];
+scenePlot(scene,'radiance energy roi',roiRect);
+ieROIDraw(scene,'shape','rectangle','shape data',roiRect);%%
+
+% The yellow must be in the plane
 thisR.set('asset', 'Area_Yellow_L', 'rotate', [-30, 0, 0]); % -5 degree around y axis
-piWRS(thisR,'render flag','hdr');
 
-%%
+% The red and blue are in the plane
 thisR.set('asset', 'Area_Red_L', 'rotate', [0, 0, 30]); % -5 degree around y axis
-piWRS(thisR,'render flag','hdr');
-
-%%
 thisR.set('asset', 'Area_Blue_L', 'rotate', [0, 0, -30]); % -5 degree around y axis
+
 piWRS(thisR,'render flag','hdr');
 
-%% Set the SPD of one of the lights
-wave = (400:10:700)';
+%% Change the SPD of the lights to halogen
 
-% halogen = ieReadSpectra('halogen_2913',wave);
-% halogen = ieReadSpectra('LED_3845',wave);
-% 
+% Setting the name is enough.  At some point we read the light file
+% and write out the values in piWRS().
 thisR.set('light','Area_Yellow_L','spd','halogen_2913');
+thisR.set('light','Area_Red_L','spd','halogen_2913');
+thisR.set('light','Area_Green_L','spd','halogen_2913');
+thisR.set('light','Area_Blue_L','spd','halogen_2913');
 
+piWRS(thisR,'render flag','hdr');
+
+%%  Now change it to LED
+
+thisR.set('light','Area_Yellow_L','spd','LED_3845');
 piWRS(thisR,'render flag','hdr');
 
 %%  Spectrum of an LED light that might be found in a car headlight
 
+%{
 % These appear about right to me (BW).
 %
 [ledSPD,wave] = ieReadSpectra('LED_3845');
@@ -116,6 +124,7 @@ xy = chromaticity(XYZ);
 hold on; plot(xy(1),xy(2),'o');
 
 % It would be good to calculate the CCT
+%}
 
 %%
 
--- utilities/texture/piTextureSet.m
+++ utilities/texture/piTextureSet.m
@@ -102,28 +102,5 @@ else
     warning('Parameter: %s does not exist in texture type: %s',...
                 pName, texture.type);    
 end
-%% Old version
-%{
-%% Parse inputs
-param = ieParamFormat(param);
-varargin = ieParamFormat(varargin);
 
-p = inputParser;
-p.addRequired('recipe', @(x)(isa(x, 'recipe')));
-p.addRequired('textureIdx');
-p.addRequired('param', @ischar);
-p.addRequired('val');
-
-p.parse(thisR, textureIdx, param, val, varargin{:});
-idx = p.Results.textureIdx;
-
-%% Conditions where we need to convert spectrum from numeric to char
-if contains(param, ['spectrum', 'rgb', 'color']) && isnumeric(val)
-    val = strrep(strcat('[', num2str(val), ']'), '  ', ' ');
-end
-
-%%
-
-thisR.textures.list{idx}.(param) = val;
-%}
 end
