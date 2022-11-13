7be6fd81f63d0646b5b882270bd3413b3b3e6a5f
David Cardinal <github@cardinalphoto.com>
2022-11-13 08:59:04 -0800

    Add test case for remoteCPUImage

--- validation/v_DockerWrapper.m
+++ validation/v_DockerWrapper.m
@@ -4,7 +4,7 @@ function result = v_DockerWrapper(options)
 % Validate various docker Wrapper calls
 %
 % Assumes that 'docker' prefs are set for
-% 
+%
 %    Variables            Stanford Defaults
 %   ---------------       -----------------
 %   remoteMachine         <servername>.stanford.edu
@@ -18,9 +18,9 @@ end
 
 %% Initialize as usual
 %
-    % iClear = getpref('ISET','initclear');
-    setpref('ISET','initclear',0);
-    ieInit;
+% iClear = getpref('ISET','initclear');
+setpref('ISET','initclear',0);
+ieInit;
 %    ...
 %    setpref('ISET','initclear',iClear);
 
@@ -52,7 +52,7 @@ try
     dockerWrapper.reset;
     ourDocker = dockerWrapper('verbosity',2,'gpuRendering',1);
     piWRS(sampleScene, 'our docker', ourDocker);
-    
+
     fprintf('succeeded\n');
     fprintf('-------------------------------------\n')
 catch
@@ -82,10 +82,7 @@ catch
     result = -1;
 end
 
-if isequal(options.length, 'long')
-%% Remote rendering (mux) CPU
-
-fprintf('*** Remote rendering on CPU ... ***\n');
+fprintf('*** Remote rendering on CPU :humanEye test... ***\n');
 fprintf('-------------------------------------\n\n')
 try
     % test remote CPU rendering
@@ -95,18 +92,56 @@ try
 
     % NOTE: To render remotely, we may need to specify the .remoteImage
     %       since we don't know what CPU it is
-    x86Image = 'digitalprodev/pbrt-v4-cpu:latest';
-    ourDocker = dockerWrapper('gpuRendering', false,...
-        'remoteImage',x86Image, 'verbosity',2);
-    piWRS(sampleScene,'our docker', ourDocker);
+    % Remote CPU for tagged case
+
+    thisDWrapper = dockerWrapper;
+    thisDWrapper.remoteCPUImage = 'digitalprodev/pbrt-v4-cpu:humanEye';
+    thisDWrapper.remoteImageTag = 'humanEye';
+    thisDWrapper.gpuRendering = 0;
+    piWRS(sampleScene,'our docker', thisDWrapper);
+
+    %{
+        DJC initial test code -- works
+    thisDWrapper = dockerWrapper('verbosity', 2, 'remoteCPUImage', ...
+        'digitalprodev/pbrt-v4-cpu:humanEye', 'remoteImageTag', 'humanEye', ...
+        'gpuRendering', 0);
+    piWRS(sampleScene,'our docker', thisDWrapper);
+    %}
 
     fprintf('succeeded\n');
     fprintf('-------------------------------------\n');
+
 catch
     fprintf(' failed\n');
     fprintf('-------------------------------------\n');
     result = -1;
 end
+
+if isequal(options.length, 'long')
+    %% Remote rendering (mux) CPU
+
+    fprintf('*** Remote rendering on CPU ... ***\n');
+    fprintf('-------------------------------------\n\n')
+    try
+        % test remote CPU rendering
+        % currently we cache 1 GPU and 1 CPU container
+        % Initializing a second one of either requires a reset
+        dockerWrapper.reset();
+
+        % NOTE: To render remotely, we may need to specify the .remoteImage
+        %       since we don't know what CPU it is
+        x86Image = 'digitalprodev/pbrt-v4-cpu:latest';
+        ourDocker = dockerWrapper('gpuRendering', false,...
+            'remoteImage',x86Image, 'verbosity',2);
+        piWRS(sampleScene,'our docker', ourDocker);
+
+        fprintf('succeeded\n');
+        fprintf('-------------------------------------\n');
+    catch
+        fprintf(' failed\n');
+        fprintf('-------------------------------------\n');
+        result = -1;
+    end
 
 end
 
