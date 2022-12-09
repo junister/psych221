4a06f5ed72b555ea0bc3daf0a1d57c4f7ba6828c
David Cardinal <github@cardinalphoto.com>
2022-12-09 14:36:01 -0800

    create asset script & assets for letters and numbers

--- scripts/characters/s_characterToAsset.m
+++ scripts/characters/s_characterToAsset.m
@@ -1,9 +1,47 @@
 %% Proto script for turning pbrt characters into
 %  assets that we can merge
 
-% Initial trial balloon
-characterRecipe = '1-pbrt.pbrt';
-thisR = piRead(characterRecipe);
+    recipeDir = piDirGet('character-recipes');
+    charAssetDir = piDirGet('character-assets');
+
+for ii = 0:9 %number assets
+    characterRecipe = [num2str(ii) '-pbrt.pbrt'];
+    thisR = piRead(characterRecipe);
+    n = thisR.get('asset names');
+
+    % Save in assets/characters instead...
+    saveFile = [erase(characterRecipe,'.pbrt') '.mat'];
+    oFile = thisR.save(fullfile(charAssetDir,saveFile));
+
+    letter = num2str(ii); % hard-code for testing
+    mergeNode = [letter,'_B'];
+    save(oFile,'mergeNode','-append');
+end
+
+% Generate letters
+Alphabet_UC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+Alphabet_LC = lower(Alphabet_UC);
+
+allLetters = [Alphabet_LC Alphabet_UC];
+
+for ii = 1:numel(allLetters)
+    characterRecipe = [allLetters(ii) '-pbrt.pbrt'];
+    thisR = piRead(characterRecipe);
+    n = thisR.get('asset names');
+
+    % Save in assets/characters instead...
+    saveFileStub = erase(characterRecipe,'.pbrt');
+    if isequal(upper(allLetters(ii)),allLetters(ii))
+        saveFile = [saveFileStub '-UC.mat'];
+    else
+        saveFile = [saveFileStub '.mat'];
+    end
+    oFile = thisR.save(fullfile(charAssetDir,saveFile));
+
+    letter = allLetters(ii); % hard-code for testing
+    mergeNode = [letter,'_B'];
+    save(oFile,'mergeNode','-append');
+end
 
 %{
 % Test to see what it looks like:
@@ -16,19 +54,6 @@ thisR = piRead(characterRecipe);
         piWRS(thisR);
 %}
 
-n = thisR.get('asset names');
-
-recipeDir = piDirGet('character-recipes');
-charAssetDir = piDirGet('character-assets');
-
-% Save in assets/characters instead...
-saveFile = [erase(characterRecipe,'.pbrt') '.mat'];
-
-oFile = thisR.save(fullfile(charAssetDir,saveFile));
-
-letter = '1'; % hard-code for testing
-mergeNode = [letter,'_B'];
-save(oFile,'mergeNode','-append');
 
 % TEST CASE BORROWED FROM LETTER SCRIPT:
 %% Merge a letter into the Chess set
@@ -50,8 +75,11 @@ piRecipeMerge(chessR,theLetter.thisR,'node name',theLetter.mergeNode);
 chessR.show('objects');
 
 to = chessR.get('to');
-chessR.set('asset','001_001_1_O','world position',to + [0 0.1 0]);
-chessR.set('asset','001_001_1_O','material name','glass');
+chessR.set('asset', '001_001_1_O', 'world position', [0 0 1]);
+%chessR.set('asset','001_001_1_O','world position',to + [0 0.1 -0.65]);
+chessR.set('asset','001_001_1_O','scale',[.3 .3 .3]);
+%chessR.set('asset','001_001_1_O','rotate',[-90 45 0]);
+chessR.set('asset','001_001_1_O','material name','brickwall001');
 piWRS(chessR,'render type','radiance');
 
 %}
--- tutorials/characters/t_sampleCharacters.m
+++ tutorials/characters/t_sampleCharacters.m
@@ -10,6 +10,9 @@ recipeSet(thisR,'lights', ourLight,'add');
 
 %% No lens or omnni camera. Just a pinhole to render a scene radiance
 
+piMaterialsInsert(thisR,'name','brickwall001');
 thisR.set('object distance',1);
 thisR.camera = piCameraCreate('pinhole'); 
+thisR.set('asset','001_001_1_O','material name','brickwall001');
+piAssetGeometry(thisR);
 piWRS(thisR);
