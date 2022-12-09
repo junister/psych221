ce31ac1bedabe7314afe2fa7a0c733cd4f118c87
David Cardinal <github@cardinalphoto.com>
2022-12-09 15:06:04 -0800

    output a sample of our character assets

--- tutorials/characters/t_sampleCharacters.m
+++ tutorials/characters/t_sampleCharacters.m
@@ -7,12 +7,28 @@ ourLight = piLightCreate(lightName,...
                         'type','distant',...
                         'cameracoordinate', true);
 recipeSet(thisR,'lights', ourLight,'add');
+piMaterialsInsert(thisR,'name','brickwall001');
+
+% Generate letters
+Alphabet_UC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+Alphabet_LC = lower(Alphabet_UC);
+letterSpacing = .9;
+
+% add letters
+for ii = 1:numel(Alphabet_LC);
+    ourLetter = num2str(Alphabet_LC(ii));
+    ourLetterAsset = piAssetLoad([ourLetter '-pbrt.mat']);
+    piRecipeMerge(thisR, ourLetterAsset.thisR);
+    if rem(ii, 2) == 0
+        thisR.set('asset',['001_001_' ourLetter '_O'],'material name','brickwall001');
+    end
+    spaceLetter = (ii - ceil(numel(Alphabet_LC)/2)) * letterSpacing;
+    thisR.set('asset',['001_001_' ourLetter '_O'],'translate', [spaceLetter 0 0]);
+end
 
 %% No lens or omnni camera. Just a pinhole to render a scene radiance
 
-piMaterialsInsert(thisR,'name','brickwall001');
-thisR.set('object distance',1);
+thisR.set('object distance',20);
 thisR.camera = piCameraCreate('pinhole'); 
-thisR.set('asset','001_001_1_O','material name','brickwall001');
-piAssetGeometry(thisR);
+%piAssetGeometry(thisR);
 piWRS(thisR);
