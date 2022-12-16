26b2772111b523964231e7f04131228608da68da
David Cardinal <github@cardinalphoto.com>
2022-12-15 17:26:44 -0800

    actual eye chart that seems  like a start

--- scripts/characters/s_eyeChart.m
+++ scripts/characters/s_eyeChart.m
@@ -20,10 +20,10 @@ chartPlacement = sceneFrom + chartDistance;
 % PS I never noticed that when getting an eye exam. Look next time!
 % at 20 feet that is 8.73mm per character.
 baseLetterSize = .00873; % 8.73mm @ 6 meters, "20/20" vision
-rowHeight = 2 * baseLetterSize;
-letterSpacing = 3 * baseLetterSize;
+rowHeight = 10 * baseLetterSize;
+letterSpacing = 6 * baseLetterSize;
 
-topRowHeight = 1; % varies with the scene we use
+topRowHeight = 1.2; % varies with the scene we use
 
 % effective distance for each row
 % need to magnify by a ratio
@@ -36,8 +36,11 @@ rowDistances = {60, 42, 24, 12, 6, 3};
 rowLetters = {'E', 'FAB', 'CDG', 'abcde', 'fghijk', 'lmnopq'};
 
 % start with a simple background
-% Replace this with a background
 thisR = piRecipeCreate('MacBethChecker');
+
+% fix defaults with our values
+thisR.set('rays per pixel', 64);
+thisR.set('filmresolution', [1280, 720]);
 
 % Set our visual "box"
 thisR = recipeSet(thisR, 'up', [0 1 0]);
@@ -52,32 +55,17 @@ for ii = 1:numel(checkerAssets)
         [0 0 chartPlacement + 2]);
 end
 
-% in case we need to delete an asset
-%origin = piAssetSearch(thisR,'object name','origin_O');
-%thisR = recipeSet(thisR,'asset',origin,'delete');
-%piWRS(thisR);
-
-
 %{
 % this is now done by RecipeCreate when we use that instead of Load
 lightName = 'from camera';
-ourLight = piLightCreate(lightName,...
-                        'type','distant',...
-                        'cameracoordinate', true);
+ourLight = piLightCreate(lightName,'type','distant','cameracoordinate', true);
 recipeSet(thisR,'lights', ourLight,'add');
 %}
-%piWRS(thisR);
 
 % Get materials we might need
 % white on black for now, need to swap
 mattewhite = piMaterialCreate('matteWhite', 'type', 'coateddiffuse');
 thisR = thisR.set('material', 'add', mattewhite);
-
-% to get glossy-black, but doesn't work
-%thisR = piMaterialsInsert(thisR,'groups',{'glossy'});
-
-%try a different material
-%piMaterialsInsert(thisR,'groups','diffuselist');
 letterMaterial = 'mattewhite'; % substitute for black
 
 % add letters by row
@@ -87,13 +75,14 @@ for ii = 1:numel(rowLetters)
     letterScale = (rowDistances{ii}/chartDistance) * baseLetterSize;
     letterVertical = topRowHeight - (ii-1) * rowHeight;
 
-    % Assume y is vertical and z is depth (not always true)
-    letterPosition = [0 letterVertical chartPlacement];
     ourRow = rowLetters{ii};
 
     for jj = 1:numel(rowLetters{ii})
        
         spaceLetter = (jj - ceil(numel(rowLetters{ii}/2))) * letterSpacing;
+
+        % Assume y is vertical and z is depth (not always true)
+        letterPosition = [spaceLetter letterVertical chartPlacement];
 
         % Need to decide on the object node name to merge
         thisR = charactersRender(thisR, rowLetters{ii}(jj), ...
@@ -101,21 +90,17 @@ for ii = 1:numel(rowLetters)
             'letterSpacing', [letterSpacing letterVertical chartDistance], ...
             'letterMaterial', letterMaterial,...
             'letterPosition', letterPosition);
-
-        % Find the name of our letter object
-        % possibly just use the final portion & then assetSearch()
-        if isequal(upper(rowLetters{ii}(jj)), rowLetters{ii}(jj))
-            letterObject = ['001_001_' lower(rowLetters{ii}(jj)) '_uc_O'];
-        else
-            letterObject = ['001_001_' rowLetters{ii}(jj) '_O'];
-        end
     end
 end
 
 %% No lens or omnni camera. Just a pinhole to render a scene radiance
 
+
 thisR.camera = piCameraCreate('pinhole');
-% Need to figure out how to set FOV to get better scaling
+
+% want a narrow FOV for the distance we're using
+% can only set once we have a pinhole camera
+thisR = thisR.set('fov',28);
 
 % Eventually want to move to human eye optics for PBRT
 %thisR.camera = piCameraCreate('human eye'); 
