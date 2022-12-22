6502f82a164c65c7ccd079d0597daaec68d03237
Brian Wandell <wandell@stanford.edu>
2022-12-22 13:17:34 -0800

    Update s_arLetters2.m

--- scripts/s_arLetters2.m
+++ scripts/s_arLetters2.m
@@ -42,7 +42,79 @@ thisR.set('to',pos(3,:));
 thisR.set('object distance',d);
 %}
 
+thisR.set('asset','distantLight_L','scale',10);
+
 scene = piWRS(thisR);
+
+% thisR.set('rays per pixel',64);
+% thisR.set('asset','distantLight_L','scale',1000);
+% thisR.set('asset','distantLight_L','remove');
+
+%% With the area light
+
+thisR = piRecipeCreate('Cornell Box Reference');
+thisR.set('film resolution',[384 256]*2);
+delta = [0.09 0 0];
+str = 'marble';
+
+% Back Wall
+idx = piAssetSearch(thisR,'object name','002_Box');
+to = thisR.get('asset',idx,'world position');
+to = to - [0.32 0.5 0.1];
+
+piMaterialsInsert(thisR,'name','wood-light-large-grain');
+thisR.set('asset',idx,'material name','wood-light-large-grain');
+
+for ii=1:numel(str), pos(ii,:) = to + ii*delta; end
+thisR = charactersRender(thisR, str,'letterSize',[0.1,0.03,0.1]*0.7,...
+    'letterRotation',[0,0,-10],'letterPosition',pos,'letterMaterial','marble-beige');
+thisR.set('skymap','sky-sunlight.exr');
+thisR.set('nbounces',4);
+thisR.set('rays per pixel',512);
+%{
+% Stare at the 'r'
+thisR.set('to',pos(3,:));
+thisR.set('object distance',2);
+%}
+
+scene = piWRS(thisR);
+
+
+% thisR.show('lights')
+% thisR.set('asset','distantLight_L','scale',10);
+
+% thisR.set('rays per pixel',64);
+% thisR.set('asset','distantLight_L','scale',1000);
+% thisR.set('asset','distantLight_L','remove');
+
+
+
+%% Two lines of text
+
+thisR = piRecipeCreate('macbeth checker');
+to = thisR.get('to') - [0.5 0 -0.8];
+delta = [0.15 0 0];
+
+str = 'Lorem'; 
+pos = zeros(numel(str),3);
+for ii=1:numel(str), pos(ii,:) = to + ii*delta; end
+pos(end,:) = pos(end,:) + delta/2;  % Move the 'm' a bit
+thisR = charactersRender(thisR, str,'letterSize',[0.15,0.1,0.15],'letterRotation',[0,15,15],...
+    'letterPosition',pos,'letterMaterial','wood-light-large-grain');
+
+str = 'ipsun'; 
+pos = pos + [0 -0.15 0];
+thisR = charactersRender(thisR, str,'letterSize',[0.15,0.1,0.15],'letterRotation',[0,15,15],...
+    'letterPosition',pos,'letterMaterial','wood-light-large-grain');
+
+thisR.set('rays per pixel',512);
+thisR.set('nbounces',4);
+
+thisR.set('skymap','sky-sunlight.exr');
+
+piWRS(thisR);
+
+% thisR.set('asset','new distant_L','scale',10);
 
 %% Contemporary bathroom
 %{
