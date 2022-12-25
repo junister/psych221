75d957b935a7f38fc6160fa1673a9d22a4f5635e
David Cardinal <github@cardinalphoto.com>
2022-12-24 17:54:58 -0800

    begin a script that will let us evaluate letters against backgrounds

--- scripts/characters/s_flashCards.m
+++ scripts/characters/s_flashCards.m
@@ -22,8 +22,30 @@ recipeSet(thisR, 'up', [0 1 0]);
 recipeSet(thisR, 'from', [0 0 0]);
 recipeSet(thisR, 'to', [0 0 5]);
 
+addMaterials(thisR)
+wallMaterial = 'wood-light-large-grain';
 piAssetSet(thisR, backWall, 'material name', wallMaterial);
 
 % I think at this point Wall is at z=2 and a,b,c are between camera & wall
 piWRS(thisR);
 thisR.birdsEye();
+
+
+
+function addMaterials(thisR)
+% should inherit from parent
+
+
+% See list of materials, if we want to select some
+allMaterials = piMaterialPresets('list');
+
+% Loop through our material list, adding whichever ones work
+for iii = 1:numel(allMaterials)
+    try
+            piMaterialsInsert(thisR, 'names',allMaterials{iii});
+    catch
+        warning('Material: %s insert failed. \n',allMaterials{ii});
+    end
+end
+end
+
