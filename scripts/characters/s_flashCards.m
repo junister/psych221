11994196594273361db8729e0811c54b6755fa95
David Cardinal <github@cardinalphoto.com>
2022-12-28 14:31:46 -0800

    minor tweaks to defaults

--- scripts/characters/s_flashCards.m
+++ scripts/characters/s_flashCards.m
@@ -25,7 +25,7 @@ humanEye = ~piCamBio(); % if using ISETBio, then use human eye
 % now we want to make the scene FOV 1 degree
 % I think we need a camera first
 thisR.camera = piCameraCreate('pinhole');
-thisR.recipeSet('fov', 50/60); % 50 arc-minutes is enough for 200/20
+thisR.recipeSet('fov', 1); % 50 arc-minutes is enough for 200/20
 
 % NOTE: We may not allow for any "padding" that is in the character
 %       assets, around the edges of the actual character.
@@ -34,7 +34,7 @@ thisR.recipeSet('fov', 50/60); % 50 arc-minutes is enough for 200/20
 % High-fidelity
 thisR.set('rays per pixel',1024);
 % Normal-fidelity
-thisR.set('rays per pixel',128);
+thisR.set('rays per pixel',256);
 
 % set our film to a square
 filmSideLength = 240;
@@ -46,7 +46,7 @@ recipeSet(thisR, 'film resolution', [filmSideLength filmSideLength]);
 % EXCEPT our Assets include blank backgrounds (Sigh)
 % Note that letter size is per our Blender assets which are l w h,
 % NOT x, y, z
-charMultiple = 10; % how many times the 20/20 version
+charMultiple = 5; % 10; % how many times the 20/20 version
 charBaseline = .00873;
 charSize = charMultiple * charBaseline;
 
@@ -60,9 +60,9 @@ charactersRender(thisR,testChars,'letterSize',[charSize .02 charSize], ...
 numMat = 0;
 
 % Can run one of the three, but maybe not all at once?
-%useCharset = Digits; % Works
+useCharset = Digits; % Works
 %useCharset = Alphabet_UC; % Works
-useCharset = Alphabet_LC; % Works
+%useCharset = Alphabet_LC; % Works
 for ii = 1:numel(useCharset)
 
     % also need to set material for letter
@@ -137,7 +137,7 @@ end
 %% Eye/Cone code grafted from s_eyeChart
 %% Need to simplify & customize
 
-function DoEyeStuff(obj, options) % TBD
+function DoEyeStuff(obj, options) 
 
 arguments
     obj;
@@ -162,97 +162,22 @@ else
         parpool('Threads');
     end
 
-    % we can render the cone mosaics, but another option is an eye model
-    useConeMosaic = true;
+    % Create the coneMosaic object
+    % We want this to be about .35mm in diameter
+    % or 1 degree FOV
+    cMosaic = coneMosaic;
+    cMosaic.fov = [1 1]; % 1 degree in each dimension
+    cMosaic.emGenSequence(50);
+    oi = oiCreate('wvf human');
 
-    if useConeMosaic
-        % Create the coneMosaic object
-        % We want this to be about .35mm in diameter
-        % or 1 degree FOV
-        cMosaic = coneMosaic;
-        cMosaic.fov = [1 1]; % 1 degree in each dimension
-        cMosaic.emGenSequence(50);
-        oi = oiCreate('wvf human');
+    oi = oiCompute(oi, scene);
+    cMosaic.name = options.thisName;
+    cMosaic.compute(oi);
+    cMosaic.computeCurrent;
 
-        oi = oiCompute(oi, scene);
-        cMosaic.name = options.thisName;
-        cMosaic.compute(oi);
-        cMosaic.computeCurrent;
+    cMosaic.window;
 
-        cMosaic.window;
 
-    else % Try Arizona eye model
 
-        thisSE = thisR;
-
-        % humaneye is part of the latest CPU docker images
-        % but is not currently supported on the GPU
-        thisDWrapper = dockerWrapper;
-        thisDWrapper.remoteCPUImage = 'digitalprodev/pbrt-v4-cpu';
-        thisDWrapper.gpuRendering = 0;
-
-        thisSE.recipe.set('render type', {'radiance','depth'});
-
-        %%  Render
-
-        scene = thisSE.render('docker wrapper',thisDWrapper);
-
-        sceneWindow(scene);
-
-        thisSE.summary;
-
-        %% Now use the optics model with chromatic aberration
-
-        % Turn off the pinhole.  The model eye (by default) is the Navarro model.
-        thisSE.set('use optics',true);
-
-        % True by default anyway
-        thisSE.set('mmUnits', false);
-
-        % We turn on chromatic aberration.  That slows down the calculation, but
-        % makes it more accurate and interesting.  We often use only 8 spectral
-        % bands for speed and to get a rought sense. You can use up to 31.  It is
-        % slow, but that's what we do here because we are only rendering once. When
-        % the GPU work is completed, this will be fast!
-
-        % Distance in meters to objects to govern accommodation.
-        %thisSE.set('to',toA); distA = thisSE.get('object distance');
-        %thisSE.set('to',toB); distB = thisSE.get('object distance');
-        %thisSE.set('to',toC); distC = thisSE.get('object distance');
-        %thisSE.set('to',toB);
-
-        % This is the distance we set our accommodation to that. Try distC + 0.5
-        % and then distA.  At a resolution of 512, I can see the difference.  I
-        % don't really understand the units here, though.  (BW).
-        %
-        % thisSE.set('accommodation',1/(distC + 0.5));
-
-        %thisSE.set('object distance',distC);
-
-        % We can reduce the rendering noise by using more rays. This takes a while.
-        thisSE.set('rays per pixel',256);
-
-        % Increase the spatial resolution by adding more spatial samples.
-        thisSE.set('spatial samples',256);
-
-        % Ray bounces
-        thisSE.set('n bounces',3);
-
-        %% Have a at the letters. Lots of things you can plot in this window.
-
-        dockerWrapper.reset();
-        thisDWrapper = dockerWrapper;
-        thisDWrapper.remoteCPUImage = 'digitalprodev/pbrt-v4-cpu';
-        thisDWrapper.gpuRendering = 0;
-        thisSE.recipe.set('render type', {'radiance','depth'});
-
-        % Runs on the CPU on mux for humaneye case.
-        oi = thisSE.render('docker wrapper',thisDWrapper);
-
-        oiWindow(oi);
-
-        % Summarize
-        thisSE.summary;
-    end
 end
 end
