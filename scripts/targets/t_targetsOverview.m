cad8a0cf5343626c66b2b1326f5dde9f9e2b81c3
Brian Wandell <wandell@stanford.edu>
2023-01-05 12:22:07 -0800

    Comments

--- scripts/targets/t_targetsOverview.m
+++ scripts/targets/t_targetsOverview.m
@@ -1,8 +1,16 @@
-%% Make some targets
+%% Make scenes with a PNG defined target on a flat surface
 %
-% Two ways
-%    1.  Textures on the flat surface
-%    2.  Place an image as a texture on the flat surface
+% There are two ways to make such images.  One is to literally use a PNG.
+% The other is to use some of the builtin PBRT methods.  This script is for
+% the PNG method.
+%
+% Materials with textures are different from simple materials.  These
+% materials have two slots
+%
+%  thisM.material
+%  thisM.texture
+%
+% The standard material is a struct with multiple slots.
 %
 
 %%
@@ -14,19 +22,25 @@ if ~piDockerExists, piDockerConfig; end
 thisR = piRecipeCreate('flat surface');
 idx = piAssetSearch(thisR,'object name','Cube');
 
-% Make the object size understandable so we see the whole shape
+% Control the Cube size so we see the whole shape
+% Units are meters, I think.
 thisR.set('asset',idx,'size',[100 1 100]);
 thisR.show('objects');
-
-% Equivalent to
-% sz = thisR.get('asset',idx,'size');
-% thisR.set('asset',idx,'scale',[100 1 100]./sz);
 
 %% This is how to make a texture
 
 % We have example png textures files in the materials/textures directory.
 % To make a material with a texture, we create the texture and the
 % material.
+%
+% To see the textures that are in there now, have a look at this:
+%
+%      dir(piDirGet('textures'))
+%
+% This could be better thought through.  For example, I think PBRT already
+% has a texture of 'checkerboard' built in, yet we have a checkerboard png
+% file (my fault, BW).
+%
 materialName = 'squarewave_h_04';   % 'squarewave_h_04';
 
 % First the texture
@@ -37,7 +51,9 @@ thisM.texture = piTextureCreate(materialName,...
     'filename', textureMap);
 
 % Then the material
-thisM.material = piMaterialCreate(materialName,'type','diffuse','reflectance val',materialName);
+thisM.material = piMaterialCreate(materialName,...
+    'type','diffuse',...
+    'reflectance val',materialName);
 
 % Finally, add the material and texture object to the recipe
 thisR.set('material', 'add', thisM);
