ddd2c841c3494bf3cd69f944836ab9bcb082fbe1
David Cardinal <github@cardinalphoto.com>
2023-01-28 05:52:27 -0800

    Revert "add Bold letter assets"
    This reverts commit 14103cf35281b8fb7b513ddacae725c787f88483.

--- scripts/text/s_characterToAsset.m
+++ scripts/text/s_characterToAsset.m
@@ -41,7 +41,8 @@ for ii = 0:9
 end
 
 %% Generate letters
-Alphabet_UC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+% H is broken
+Alphabet_UC = 'ABCDEFGIJKLMNOPQRSTUVWXYZ';
 Alphabet_LC = lower(Alphabet_UC);
 
 allLetters = [Alphabet_LC Alphabet_UC];
@@ -56,52 +57,6 @@ for ii = 1:numel(allLetters)
     else
         characterRecipe = [allLetters(ii) '-pbrt.pbrt'];
     end
-
-    if ~exist(characterRecipe,'file')
-        warning("Help! %s\n", characterRecipe)
-    end
-    thisR = piRead(characterRecipe);
-
-    % We do not want the input file to have the user's full path.  So we
-    % reduce it to just the name of the pbrt file.
-    thisR.set('inputfile',characterRecipe);
-
-    % Assets do not get rendered directly.  They get merged into another
-    % scene which has its own output. So they do not need an output file or
-    % directory.
-    thisR.set('outputfile','');
-
-    % piRead changes asset names to lower case
-    % This means things break when we merge UC letters into recipes
-    
-    n = thisR.get('asset names');
-
-    % Save in assets/characters instead...
-    saveFileStub = erase(characterRecipe,'.pbrt');
-    saveFile = [saveFileStub '.mat'];
-    %{
-    % not sure why we needed this?
-    if isequal(upper(allLetters(ii)),allLetters(ii))
-        saveFile = [saveFileStub '.mat'];
-    else
-        saveFile = [saveFileStub '.mat'];
-    end
-    %}
-    oFile = thisR.save(fullfile(charAssetDir,saveFile));
-
-    letter = allLetters(ii); % hard-code for testing
-    mergeNode = [letter,'_B'];
-    save(oFile,'mergeNode','-append');
-end
-
-% UC-Courier-Bold
-for ii = 1:numel(Alphabet_UC)
-    disp(ii);
-%    if isequal(upper(allLetters(ii)),allLetters(ii))
-        characterRecipe = [lower(allLetters(ii)) '_UC-Courier-Bold-pbrt.pbrt'];
-%    else
-%        characterRecipe = [allLetters(ii) '-pbrt.pbrt'];
-%    end
 
     if ~exist(characterRecipe,'file')
         warning("Help! %s\n", characterRecipe)
