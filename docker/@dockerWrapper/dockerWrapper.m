435c9ac41ea98658d3296e0f0141c338cb346cec
David Cardinal <github@cardinalphoto.com>
2023-02-28 11:02:00 -0800

    note that restart check takes some time

--- docker/@dockerWrapper/dockerWrapper.m
+++ docker/@dockerWrapper/dockerWrapper.m
@@ -637,7 +637,9 @@ classdef dockerWrapper < handle
                     else
                         cFlag = '';
                     end
-
+                    
+                    % this check can take close to a second on a remote
+                    % connection. Don't know if it is really worth doing?
                     [~, result] = system(sprintf("docker %s ps | grep %s", cFlag, obj.staticVar('get','PBRT-CPU', '')));
                     if strlength(result) == 0
                         obj.staticVar('set','PBRT-CPU', obj.startPBRT('CPU'));
