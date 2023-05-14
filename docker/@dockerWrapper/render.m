4724f3c259cce76b368e476a51d4a1a42277cc97
David Cardinal <github@cardinalphoto.com>
2023-05-14 14:31:16 -0700

    fix getsymlink() tp properly add lenses

--- docker/@dockerWrapper/render.m
+++ docker/@dockerWrapper/render.m
@@ -228,13 +228,13 @@ fprintf('\n------------------\n');
 end
 
 function getLinks = getSymLinks()
-geoCommand = 'cp -n -r geometry/* /ISETResources/geometry ; rm -rf geometry ; ln -s /ISETResources/geometry geometry';
-texCommand = 'cp -n -r textures/* /ISETResources/textures ; rm -rf textures ; ln -s /ISETResources/textures textures';
-spdCommand = 'cp -n -r spds/* /ISETResources/spds ; rm -rf spds ; ln -s /ISETResources/spds spds';
-lgtCommand = 'cp -n -r lights/* /ISETResources/lights ; rm -rf lights ; ln -s /ISETResources/lights lights';
-skyCommand = 'cp -n -r skymaps/* /ISETResources/skymaps ; rm -rf skymaps ; ln -s /ISETResources/skymaps skymaps';
+geoCommand =  'cp -n -r geometry/* /ISETResources/geometry ; rm -rf geometry ; ln -s /ISETResources/geometry geometry';
+texCommand =  'cp -n -r textures/* /ISETResources/textures ; rm -rf textures ; ln -s /ISETResources/textures textures';
+spdCommand =  'cp -n -r spds/* /ISETResources/spds ; rm -rf spds ; ln -s /ISETResources/spds spds';
+lgtCommand =  'cp -n -r lights/* /ISETResources/lights ; rm -rf lights ; ln -s /ISETResources/lights lights';
+skyCommand =  'cp -n -r skymaps/* /ISETResources/skymaps ; rm -rf skymaps ; ln -s /ISETResources/skymaps skymaps';
 lensCommand = 'cp -n -r lens/* /ISETResources/lens ; rm -rf lens ; ln -s /ISETResources/lens lens';
-getLinks =  sprintf(' %s ;  %s ; %s ; %s ; %s %s', ...
+getLinks =  sprintf(' %s ;  %s ; %s ; %s ; %s ; %s', ...
     geoCommand, texCommand, spdCommand, lgtCommand, skyCommand, lensCommand);
 end
 
