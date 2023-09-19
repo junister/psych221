c1c876b84bb7fa297cba1e34afb583ec4a58f830
David Cardinal <github@cardinalphoto.com>
2023-09-19 05:46:02 -0700

    add more material choices, move sphere right up to light

--- tutorials/t_radianceIrradiance.m
+++ tutorials/t_radianceIrradiance.m
@@ -13,42 +13,54 @@ if ~piDockerExists, piDockerConfig; end
 
 %% Create recipe
 
-thisR = piRecipeCreate('sphere');
+thisR = piRecipeCreate('Sphere');
+%thisR = piRecipeCreate('flatsurface');
 thisR.show('lights');
 
 thisR.set('lights','all','delete');
-% spot light doesn't seem to be working
-thisLight = piLightCreate('area light', 'type','area','rgb spd',[1 1 1]);
+
+thisLight = piLightCreate('spot light', 'type','spot','rgb spd',[100 100 100]);
 thisLight.cameracoordinate = true; % not set by default
 
 thisR.set('lights',thisLight,'add');
 thisR.show('lights');
 
-% thisR.set('skymap','room.exr');
+%thisR.set('skymap','room.exr');
 
-% A low resolution rendering for speed
-thisR.set('film resolution',[200 150]);
-thisR.set('rays per pixel',256);
-thisR.set('nbounces',10); 
+thisR.set('film resolution',[400 300]);
+thisR.set('rays per pixel',512);
+thisR.set('nbounces',3); 
 thisR.set('fov',45);
-piWRS(thisR,'name','diffuse');
 
-% To insert one of those materials into the recipe, 
-mirrorName = 'mirror';
-piMaterialsInsert(thisR,'name',mirrorName);
-
-% Assigning mirror to sphere
+% try moving the subject really close
 assetSphere = piAssetSearch(thisR,'object name','Sphere');
-thisR.set('asset', assetSphere, 'material name', mirrorName);
+piAssetTranslate(thisR,assetSphere,[0 0 -298]);
+
+piWRS(thisR,'name','diffuse','mean luminance', -1); % works
+
+
+piMaterialsInsert(thisR,'name','mirror'); % fail
+piMaterialsInsert(thisR,'name','glass'); % fail
+piMaterialsInsert(thisR,'name','glass-f5'); % fail
+piMaterialsInsert(thisR,'name','metal-ag'); % fail
+piMaterialsInsert(thisR,'name','rough-metal'); % fail
+piMaterialsInsert(thisR,'name','chrome'); % fail
+piMaterialsInsert(thisR,'name','glossy-red'); % works
+
+% To use one of those materials into the recipe: 
+useMaterial = 'glass-f5';
+
+% Assigning new surface to sphere
+thisR.set('asset', assetSphere, 'material name', useMaterial);
 
 %%
 % add a skymap as a test
 fileName = 'room.exr';
-%thisR.set('skymap',fileName);
+%thisR.set('skymap',fileName); % works
 
 piWrite(thisR);
 %piWRS(thisR,'name','mirror','render flag','hdr');
-scene = piRender(thisR);
+scene = piRender(thisR, 'mean luminance',-1);
 sceneWindow(scene);
 
 
