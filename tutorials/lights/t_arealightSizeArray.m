e0b0bcda385b40e5dcf23d3300e814730bc19c9a
Brian Wandell <1837145+wandell@users.noreply.github.com>
2023-11-13 11:27:15 -0800

    Specializing the individual scripts

--- tutorials/lights/t_arealightSizeArray.m
+++ tutorials/lights/t_arealightSizeArray.m
@@ -1,10 +1,8 @@
-%% t_arealightSizeArray.m
+%% t_arealightArray.m
 %
-%  * Change the size (scale) of an area light 
-%  * Move the light closers and further from the target
-%  * Change the spread of the light
-%  * Combine a couple of area lights into an array
-%  * Move the camera so that it looks at the area light array
+%  * Create a triangular array of area lights
+%  * Move the surface closer to the lights to see them
+%  * Create a circular array of lights
 %
 % See also
 %  t_arealight*
@@ -20,39 +18,6 @@ if ~piDockerExists, piDockerConfig; end
 thisR = piRecipeCreate('flat surface');
 thisR.set('rays per pixel',128);
 
-%% Make an area light
-
-% The light position is (0,0,0), which happens to be the camera position
-area{1} = piLightCreate('area1',...
-    'type','area',...
-    'spd spectrum','D65');
-thisR.set('lights',area{1},'add');
-thisR.show('lights');
-
-% Rotate the light so it is pointing at the surface
-thisR.set('light','area1','rotate',[0 180 0]);
-
-% The light is very big so it illuminates the whole surface
-piWRS(thisR,'name','big light');
-
-%% Reduce the size of the light and its spread
-
-thisR.set('light','area1','spread',10);
-thisR.set('light','area1','shape scale',0.005);   % Five millimeters
-
-piWRS(thisR,'name','small light');
-
-%% Return the size
-
-thisR.set('light','area1','shape scale',100);
-piWRS(thisR,'name','restore size');
-
-%% Create an array with different positions 
-
-% Start fresh with the scene.  Not necessary, but ...
-thisR = piRecipeCreate('flat surface');
-thisR.set('rays per pixel',128);
-
 % The flat surface object is called Cube.  It is 1m in size.  I shrink it
 % so we can also see the environment light, later.
 cubeID = piAssetSearch(thisR,'object name','Cube');
@@ -62,7 +27,7 @@ thisR.set('asset',cubeID,'scale',0.25);
 piMaterialsInsert(thisR,'names',{'mirror','diffuse-white','marble-beige','wood-mahogany'});
 thisR.set('asset',cubeID,'material name', 'diffuse-white');
 
-% Add the three area lights
+% The three area lights
 area = cell(1,3);
 
 % Triangular positions, a few millimeters off to the side of the
@@ -71,7 +36,7 @@ pos = [0.005 0 0;
        0.100 0 0; 
        0.05 0.050 0];
 
-% Set some of the light parameters
+% Create and set the light parameters
 for ii=1:3
     area{ii} = piLightCreate(sprintf('area-%d',ii),...
         'type','area',...
@@ -93,53 +58,19 @@ piWRS(thisR);
 
 %% Move the cube closer to the camera
 
+% This makes it easier to see the three light sources
 thisR.set('asset',cubeID,'translate',[0 0 -0.5]);
 piWRS(thisR);
 
-%% Change the light size and compensate by scaling the SPD
+%% Create a ring of light sources
 
-% Start fresh with a small Cube
-ieInit;
-clear area;
+% Make the lights.  They will be in a circle around the camera.
+% The camera is pointed in this direction.
+direction = thisR.get('fromto');
+[pts, radius] = piRotateFrom(thisR, direction, ...
+    'n samples',nLights+1, ...
+    'radius',velscopeRadius,...
+    'show',false);
 
-% Start fresh with the scene.  Not necessary, but ...
-thisR = piRecipeCreate('flat surface');
-thisR.set('rays per pixel',128);
-specScale = 50;
 
-area{1} = piLightCreate('area1',...
-    'type','area',...
-    'spd spectrum','D65', ...
-    'specscale',specScale);
-thisR.set('lights',area{1},'add');
-thisR.set('light','area1','spread',5);  % Narrow spread so the size will be easier to see
-thisR.set('light','area1','rotate',[0 180 0]);
-thisR.show('lights');
-
-% We cannot use the shape scale parameter as part of the create
-% because it is a method, not a parameter.  That could be changed by
-% adding it to piLightCreate for the area light.
-thisR.set('light',area{1},'shape scale',0.1);
-
-% Reduce the light's size a couple of times.  We change the SPD
-% scaling and Shape scaling together.
-scene = piWRS(thisR,'mean luminance',-1,'render flag','rgb');
-fprintf('Mean (max) luminance: %.4g (%.4g)\n',...
-    sceneGet(scene,'mean luminance'), ...
-    sceneGet(scene,'max luminance'));
-
-thisR.set('light',area{1},'shape scale',0.3);
-thisR.set('light',area{1},'specscale',specScale/(0.3)^2);
-scene = piWRS(thisR,'mean luminance',-1,'render flag','rgb');
-fprintf('Mean (max) luminance: %.4g (%.4g)\n',...
-    sceneGet(scene,'mean luminance'), ...
-    sceneGet(scene,'max luminance'));
-
-thisR.set('light',area{1},'shape scale',0.3);
-thisR.set('light',area{1},'specscale',specScale/(0.3*0.3)^2);
-
-scene = piWRS(thisR,'mean luminance',-1,'render flag','rgb');
-fprintf('Mean (max) luminance: %.4g (%.4g)\n',...
-    sceneGet(scene,'mean luminance'), ...
-    sceneGet(scene,'max luminance'));
 %%
