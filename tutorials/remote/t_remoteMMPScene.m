8001159a8348e64e196b21a071d8b7fc3075f14b
Brian Wandell <1837145+wandell@users.noreply.github.com>
2024-02-01 12:02:03 -0800

    Update t_remoteMMPScene.m

--- tutorials/remote/t_remoteMMPScene.m
+++ tutorials/remote/t_remoteMMPScene.m
@@ -1,7 +1,19 @@
 % Quick example of remote rendering one of the scenes
 % from Matt Pharr's repo:
+%
+% DJC
 
+%%
 thisR = piRecipeDefault("scene name",'bmw-m6');
+
+% You can adjust the render parameters, such as
+% thisR.set('rays per pixel',1024)
+
+%% Render
 piWrite(thisR);
 scene = piRender(thisR,'remotescene',true);
 sceneWindow(scene);
+
+%% You can also execute this the usual way as
+% piWRS(thisR,'remotescene',true);
+
